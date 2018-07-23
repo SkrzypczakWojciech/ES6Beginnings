78ef41af900d156c88f8fffd562c55f96c507773
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-07-23 11:29:16 +0200

    change in zad2,3,4

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ let second = `World`;
 
 document.write('zadanie1 :' +`${first} ${second}`+  '<br>');
 
-function multiply(number1 = '1',number2 = '1') {
-	let score = number1*number2
-	document.write('zadanie2: '+ score + '<br>')
-}
-multiply(8)
 
-var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+const multiply = (x = 1,y = 1) => x * y;
+document.write('zadanie2: '+ multiply(7) + '<br>')
+
+var total = 0;
+var arguments = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+const avg = (...args) => args.forEach(arg => total +=);
+document.write('zadanie3i4: '+ avg + '<br>')
+
+
+/*var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
 var total = 0;
 for(var i = 0; i < grades.length; i++) {
     total += grades[i];
 }
 var avg = total / grades.length;
 document.write('zadanie3i4: '+ avg + '<br>')
+*/
 
 const array = [1, 4, 'Iwona', false, 'Nowak']
 const [,,FirstName,,SecondName] = array
